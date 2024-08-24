80c12612aabafdbb55f807e58ff810b8fd26b7a4
root <root@ip-172-31-7-72.ap-south-1.compute.internal>
2024-08-24 09:08:57 +0000

    Trigger the job

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 echo "Hello world"
 echo "Welcome to Guvi Zen_Devops program"
-echo "Check the email for the jenkins pipeline success confirmation"
+echo "Check the email for the jenkins pipeline job status"
+echo "Finally testing the job"
